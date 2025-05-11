6a34a69dcb3d13522d109f169ba5fa1763cc72af
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2025-05-12 02:29:15 +0300

    app uplode 1 image with text

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class post(models.Model):
+    title=models.CharField(max_length=200)
+    image=models.ImageField(upload_to="photos/%y/%m/%d",default='0.jpg')
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import  post
 # Create your views here.
-
-def home():
-    pass
+def index(request):
+    # return HttpResponse("ffffffffffff")
+    data=post.objects.all()
+    return render(request,"index.html",{"data":data})
